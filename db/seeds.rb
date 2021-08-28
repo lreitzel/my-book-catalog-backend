8bfd4faf4633854467d1a615e6d90db4c82bc88a
Lydia <lreitzel@gmail.com>
2021-08-27 20:01:48 -0400

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-12.times do
-    Genre.create(name: Faker::Book.unique.genre)
-end
+# mystery = Genre.create(name: "Mystery") 
+# thriller = Genre.create(name: "Thriller") 
+# horror = Genre.create(name: "Horror")
+# historical = Genre.create(name: "Historical")
+# romance = Genre.create(name: "Romance")
+# western = Genre.create(name: "Western")
+# scienceFiction = Genre.create(name: "Science Fiction")
+# dystopian = Genre.create(name: "Dystopian")
+# magicalRealism = Genre.create(name: "Magical Realism")
 
-20.times do
-    gen_id = rand(1..12)
-    Book.create(title: Faker::Book.title, author: Faker::Book.author, read: true, genre_id: gen_id)
-end
+
+# 20.times do
+#     gen_id = rand(13..21)
+#     Book.create(title: Faker::Book.title, author: Faker::Book.author, read: true, genre_id: gen_id)
+# end
 
